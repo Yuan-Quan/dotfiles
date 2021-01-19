760411b9f348e247b72d6af5b93bb5b086104134
luna <metroyuan@gmail.com>
2021-01-19 18:25:51 +0800

    added struts control keybind && use additionalkeysp

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -22,6 +22,11 @@ import XMonad.Layout.Grid
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
+
 
 import XMonad.Actions.PhysicalScreens
 import XMonad.Actions.CycleWS
@@ -30,12 +35,15 @@ import qualified XMonad.Actions.DynamicWorkspaceOrder as DO
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageDocks
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Scratchpad
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.WorkspaceCompare
 import XMonad.Util.SpawnOnce
+
+import XMonad.Util.EZConfig (additionalKeysP)
 
 import qualified XMonad.StackSet as W
 
@@ -49,46 +57,23 @@ myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#FF69B4"
 myTerminal    = "/usr/bin/alacritty" -- preferred terminal emulator
 
---
 -- key bindings
---
 
-myKeys = [
-   ((myModMask, xK_a), sendMessage MirrorShrink) -- for  ResizableTall
- , ((myModMask, xK_z), sendMessage MirrorExpand) -- for  ResizableTall
- -- selecting particular monitors
- , ((myModMask, xK_w), viewScreen def 0)
- , ((myModMask, xK_e), viewScreen def 1)
- , ((myModMask, xK_r), viewScreen def 2)
- -- cycling through workspaces in multi monitor setup, skipping scratchpad
- , ((myModMask .|. mod5Mask, xK_h), prevHiddenNonEmptyNoSPWS)
- , ((myModMask, xK_Left),           prevHiddenNonEmptyNoSPWS)
- , ((myModMask .|. mod5Mask, xK_l), nextHiddenNonEmptyNoSPWS)
- , ((myModMask, xK_Right),          nextHiddenNonEmptyNoSPWS)
- , ((myModMask, xK_o), scratchPad)
- --((myModMask, xK_d), spawn "/home/martin/bin/qstardict-show-hide.sh")
- ]
- where
-   scratchPad = scratchpadSpawnActionTerminal myTerminal
-   getSortByIndexNoSP = fmap (.scratchpadFilterOutWorkspace) getSortByIndex
-   prevHiddenNonEmptyNoSPWS = windows . W.greedyView =<< findWorkspace getSortByIndexNoSP Prev HiddenNonEmptyWS 1
-   nextHiddenNonEmptyNoSPWS = windows . W.greedyView =<< findWorkspace getSortByIndexNoSP Next HiddenNonEmptyWS 1
+myKeys :: [([Char], X ())]
+myKeys =
+  --xmonad
+  [ ("M-C-r", spawn "xmonad --recompile") -- Recompiles xmonad]
+  -- Layouts
+  , ("M-<Space>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts) -- Toggles noborder/full
+  , ("M-S-<Space>", sendMessage ToggleStruts)     -- Toggles struts
+  , ("M-S-n", sendMessage $ MT.Toggle NOBORDERS)  -- Toggles noborder
+  ] 
 
 -- key bindings used only in stand alone mode (without KDE)
-myStandAloneKeys = [
-   ((myModMask, xK_x),             spawn "xscreensaver-command -lock")
- , ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight -inc 10")
- , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
- , ((0, xF86XK_AudioRaiseVolume),  spawn "amixer -D pulse sset Master 10%+")
- , ((0, xF86XK_AudioLowerVolume),  spawn "amixer -D pulse sset Master 10%-")
- , ((0, xF86XK_AudioMute),         spawn "amixer -D pulse sset Master toggle")
- ]
+myStandAloneKeys :: [([Char], X ())]
+myStandAloneKeys = [ ]
 
---
 -- hooks for newly created windows
--- note: run 'xprop WM_CLASS' to get className
---
-
 myManageHook :: ManageHook
 myManageHook = manageDocks <+> manageScratchPad <+> coreManageHook
 
@@ -124,7 +109,7 @@ myStartupHook = do
 --     spawnOnce "/usr/bin/yakuake"
 
 -- layout hooks
-myLayoutHook = spacingRaw True (Border 0 6 6 6) True (Border 6 6 6 6) True $
+myLayoutHook = (spacingRaw True (Border 0 6 6 6) True (Border 6 6 6 6) True) $
              avoidStruts $ coreLayoutHook
 
 coreLayoutHook = tiled ||| Mirror tiled ||| Full ||| Grid
@@ -147,9 +132,7 @@ myLogHook xmproc = dynamicLogWithPP xmobarPP
   , ppTitle  = xmobarColor "green" "" . shorten 50
   }
 
---
 -- desktop :: DESKTOP_SESSION -> desktop_configuration
---
 
 desktop "gnome"         = gnomeConfig
 desktop "xmonad-gnome"  = gnomeConfig
@@ -176,7 +159,7 @@ main = do
         , startupHook = myStartupHook
         , layoutHook  = myLayoutHook
         , manageHook  = myManageHook <+> manageHook defDesktopConfig
-        } `additionalKeys` myKeys
+        } `additionalKeysP` myKeys
   -- when running standalone (no KDE), try to spawn xmobar (if installed)
   xmobarInstalled <- doesFileExist "/usr/bin/xmobar"
   if session == Just "xmonad" && xmobarInstalled
@@ -184,5 +167,5 @@ main = do
             xmonad $ myDesktopConfig
               { logHook  = myLogHook mproc
               , terminal = myTerminal
-              } `additionalKeys` myStandAloneKeys
+              } `additionalKeysP` myStandAloneKeys
     else do xmonad myDesktopConfig
