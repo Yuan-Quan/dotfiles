03eed9ad19e876ff8266a2bf9d6bedb5a1d880ea
luna <metroyuan@gmail.com>
2021-01-28 23:34:25 +0800

    update fish_user_paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 # Color script when open it
 colorscript random
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias rya="google-chrome-stable --app=http://localhost:2017"
