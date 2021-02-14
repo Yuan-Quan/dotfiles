c384ee20c4a76c11291ef79519ee4bbc7721e2f0
luna <metroyuan@gmail.com>
2021-02-14 11:41:07 +0800

    added script to set proxy

---
+++ proxy.sh
@@ -0,0 +1,10 @@
+export http_proxy=http://127.0.0.1:7890/
+export https_proxy=$http_proxy
+export ftp_proxy=$http_proxy
+export rsync_proxy=$http_proxy
+export no_proxy="localhost,127.0.0.1,localaddress,.localdomian.com"
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+export FTP_PROXY=$ftp_proxy
+export RSYNC_PROXY=$rsync_proxy
+export NO_PROXY=$no_proxy
