65bb2b26f87463abdc4c2fdf8d80862cb4661ba2
Luna Child <metroyuan@gmail.com>
2021-04-09 12:51:14 +0800

    removed alas for rya

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
 # Color script when open it
 colorscript random
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
-alias rya="google-chrome-stable --app=http://localhost:2017"
