aa33a47ab107005c1601fe37e941b3098d95f940
Reimu Hakurei <reimu@HakureiShrine.localdomain>
2021-06-28 10:47:05 +0800

    set font and font size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,3 +6,5 @@
 (tooltip-mode -1)                          ; Disable tooltip
 (menu-bar-mode -1)                         ; Disable menubar
 (set-fringe-mode 10)                       ; Give it some breathing room
+
+(set-face-attribute 'default nil :font "FiraMono Nerd Font Mono" :height 128)       ; Set font and font size
