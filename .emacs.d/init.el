4287a0d692a937f7a429729426d916dc662c44d4
Reimu Hakurei <reimu@HakureiShrine.localdomain>
2021-06-28 11:00:19 +0800

    Init package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,3 +8,21 @@
 (set-fringe-mode 10)                       ; Give it some breathing room
 
 (set-face-attribute 'default nil :font "FiraMono Nerd Font Mono" :height 128)       ; Set font and font size
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
