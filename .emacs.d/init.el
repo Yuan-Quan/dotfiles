8ad1a429b1a4bed7ad4685701adc82f45277dd33
Reimu Hakurei <reimu@HakureiShrine.localdomain>
2021-06-28 10:39:06 +0800

    clean up interface

---
+++ .emacs.d/init.el
@@ -0,0 +1,8 @@
+
+(setq inhibit-startup-message t)           ; Make startup clean
+
+(scroll-bar-mode -1)                       ; Disable scroll bar
+(tool-bar-mode -1)                         ; Disable toolbar
+(tooltip-mode -1)                          ; Disable tooltip
+(menu-bar-mode -1)                         ; Disable menubar
+(set-fringe-mode 10)                       ; Give it some breathing room
