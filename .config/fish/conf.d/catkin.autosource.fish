a243a9ae37c2d5e9941f534840ef73c88f6c9df5
Luna Child <metroyuan@gmail.com>
2022-02-23 11:14:20 +0800

    auto source setup for catkin workspace

---
+++ .config/fish/conf.d/catkin.autosource.fish
@@ -0,0 +1,11 @@
+function catkinSource --on-variable PWD
+    status --is-command-substitution; and return
+    if test -e ".catkin_workspace"
+        bass source devel/setup.bash
+	set_color green
+        echo -n "[catkin_fish]"
+	set_color normal
+	echo -n " sourced setup for current workspace"
+	echo ""
+    end
+end
