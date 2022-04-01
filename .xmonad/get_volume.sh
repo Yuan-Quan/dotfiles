54e23e46f04006bc2b31dc0278f7d76df5ab81ab
Luna Child <metroyuan@gmail.com>
2022-04-01 20:58:45 +0800

    fixed incorrect output

--- .xmonad/get_volume.sh
+++ .xmonad/get_volume.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-getdefaultsinkname() {
-    pacmd stat | awk -F": " '/^Default sink name: /{print $2}'
-}
-
-getdefaultsinkvol() {
-    pacmd list-sinks |
-        awk '/^\s+name: /{indefault = $2 == "<'$(getdefaultsinkname)'>"}
-            /^\s+volume: / && indefault {print $5; exit}'
-
-}
-#getdefaultsinkname
-getdefaultsinkvol
+pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
 
